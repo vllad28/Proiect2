a258859f52b233b8b10fb44b14520970e295c9a1
VLAD <vlad.stanescu@student.upt.ro>
2025-01-16 15:08:08 +0200

    Vagon

---
+++ ProiectPooGestionareTrenuri/Vagon.cs
@@ -0,0 +1,20 @@
+﻿namespace ProiectPooGestionareTrenuri;
+
+public enum TipLoc
+{
+    Clasa1,
+    Clasa2,
+    Compartiment,
+    VagonRestaurant
+}
+public class Vagon
+{
+    public TipLoc TipVagon { get; set; }
+    public int NumarLocuriDisponibile { get; set; }
+
+    public Vagon(TipLoc tipVagon, int locuri)
+    {
+        TipVagon = tipVagon;
+        NumarLocuriDisponibile = locuri;
+    }
+}
