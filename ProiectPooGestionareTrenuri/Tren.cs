2bd6b354b27393d2908c60984797ca11493c8cd9
Maxim <maxim.stavenschi@student.upt.ro>
2025-01-16 14:57:31 +0200

    clasa tren

---
+++ ProiectPooGestionareTrenuri/Tren.cs
@@ -0,0 +1,41 @@
+﻿namespace ProiectPooGestionareTrenuri;
+public enum TipTren
+{
+    Regio,
+    InterRegio,
+    Express
+}
+
+public   class Tren
+{
+    public string Id { get; set; }
+    public int NumarTren { get; set; }
+    public TipTren Tip { get; set; }
+    public int Capacitate { get; set; }
+    public List<Vagon> Vagoane { get; set; }
+
+    public Tren(string id, int numar, TipTren tip, int capacitate)
+    {
+        Id = id;
+        NumarTren = numar;
+        Tip = tip;
+        Capacitate = capacitate;
+        Vagoane = new List<Vagon>();
+
+    }
+
+
+
+    public void AdaugareVagon(Vagon vagon)
+    {
+        Vagoane.Add(vagon);
+    }
+}
+PRET:
+namespace ConsoleApp4;
+
+public abstract class Pret
+{
+    public abstract double CalculeazaPret(double pretBaza);
+}
+}
