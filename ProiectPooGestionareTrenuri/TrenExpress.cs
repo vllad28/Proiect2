db9dcd194e2c5c22f94408574a1c07ff73bdab61
Maxim <maxim.stavenschi@student.upt.ro>
2025-01-16 15:04:30 +0200

    make a classes trenexpres and trenregio

---
+++ ProiectPooGestionareTrenuri/TrenExpress.cs
@@ -0,0 +1,13 @@
+﻿namespace ProiectPooGestionareTrenuri;
+
+public abstract class TrenExpress :Pret
+{
+
+    public double TaxaServiciuMasa { get; set; } = 75.0;
+
+    public override double CalculeazaPret(double pretBaza)
+    {
+        return pretBaza; // Fără reducere
+    }
+
+}
