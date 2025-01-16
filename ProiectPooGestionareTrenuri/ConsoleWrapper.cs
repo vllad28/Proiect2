1eeb0b9b0f097209594fc99892968e96f6040657
VLAD <vlad.stanescu@student.upt.ro>
2025-01-16 15:09:34 +0200

    ConsoleWrapper

---
+++ ProiectPooGestionareTrenuri/ConsoleWrapper.cs
@@ -0,0 +1,39 @@
+﻿namespace ProiectPooGestionareTrenuri;
+
+public class ConsoleWrapper
+{
+    public virtual void WriteLine(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public virtual string ReadLine(string message = "")
+    {
+        if (!string.IsNullOrEmpty(message))
+        {
+            WriteLine(message);
+        }
+        return Console.ReadLine()?.Trim();
+    }
+
+    public virtual int ReadInt(string message)
+    {
+        while (true)
+        {
+            WriteLine(message);
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                return result;
+            }
+            WriteLine("Input-ul nu este un numar valid! Incercati din nou.");
+        }
+    }
+
+    public virtual void WriteList<T>(IEnumerable<T> items)
+    {
+        foreach (var item in items)
+        {
+            WriteLine(item?.ToString());
+        }
+    }
+}
