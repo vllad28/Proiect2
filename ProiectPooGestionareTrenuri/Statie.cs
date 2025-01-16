0c06d86e1c6e6ed8249d58ef72900971ad3f3592
RadAdrianIonut <adrian.rad@student.upt.ro>
2025-01-16 14:54:36 +0200

    Definire Statii

---
+++ ProiectPooGestionareTrenuri/Statie.cs
@@ -0,0 +1,13 @@
+﻿namespace ProiectPooGestionareTrenuri;
+
+public class Statie
+{
+    public string NumeStatie { get; set; }
+    public TimeSpan TimpDeStop { get; set; }
+
+    public Statie(string nume, TimeSpan timp)
+    {
+        NumeStatie = nume;
+        TimpDeStop = timp;
+    }
+}
