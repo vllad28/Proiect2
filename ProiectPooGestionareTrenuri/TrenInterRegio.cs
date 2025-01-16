7122f95d07bd16509dd9cd9a0fa57dd1bd283b10
Maxim <maxim.stavenschi@student.upt.ro>
2025-01-16 15:06:16 +0200

    creeare clasa Treninterregio

---
+++ ProiectPooGestionareTrenuri/TrenInterRegio.cs
@@ -0,0 +1,11 @@
+﻿namespace ProiectPooGestionareTrenuri;
+
+public abstract class TrenInterRegio :Pret
+{
+    public double TaxaCompartimentPrivat { get; set; } = 50.0;
+
+    public override double CalculeazaPret(double pretBaza)
+    {
+        return pretBaza; // Fără reducere
+    }
+}
