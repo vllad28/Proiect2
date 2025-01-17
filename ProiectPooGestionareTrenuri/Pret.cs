3bc491b27b55bd729e31d9a0bca77cca6976ab95
Maxim <maxim.stavenschi@student.upt.ro>
2025-01-17 09:03:18 +0200

    am facut o schimbare

--- ProiectPooGestionareTrenuri/Pret.cs
+++ ProiectPooGestionareTrenuri/Pret.cs
@@ -4,13 +4,5 @@ public abstract class Pret
 {
     public abstract double CalculeazaPret(double pretBaza);
 }
-TrenRegio:
-namespace ConsoleApp4;
 
-public abstract class TrenRegio :Pret
-{
-    public override double CalculeazaPret(double pretBaza)
-    {
-        return pretBaza * 0.8; // Reducere de 20%
-    }
-}
+
